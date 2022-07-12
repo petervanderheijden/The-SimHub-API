38ee1ea38ac3a4dc7f47a9704ec94e0935f754ca
Peter van der Heijden <enzopeter02@gmail.com>
2022-07-13 01:14:46 +0200

    initial commit

---
+++ src/Modules/express.js
@@ -0,0 +1,15 @@
+function express() {
+    const express = require('express');
+    const app = express();
+    let port = process.env.PORT || 3000
+
+    app.get('/', (req, res) => {
+        res.send('Hello, world!');
+    });
+
+    app.listen(port, () => {
+        console.log('ff')
+    });
+}
+
+export { express };
---
+++ src/server.js
@@ -0,0 +1,3 @@
+import * as express from "./Modules/express.js";
+
+express.express();
